d67c048da8c907b982fe2fe6a6edf8d9c9c611da
Fede <fedebenitez1696@gmail.com>
2020-09-28 15:46:16 -0300

    archivos de autenticacion y handlebars agregados

--- src/index.js
+++ src/index.js
@@ -1,23 +1,49 @@
 const express = require("express");
 const morgan = require("morgan");
+const expresshbs = require("express-handlebars");
+const path = require("path");
 
 //Inicializaciones
 const app = express();
 
 //Configuraciones
 app.set("port", process.env.PORT || 4000);
+//ruta de la carpeta views
+app.set("views", path.join(__dirname, "views"));
 
+app.engine(
+  ".hbs",
+  expresshbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: ".hbs",
+    helpers: require("./lib/handlebars")
+  })
+);
+
+app.set("view engine", ".hbs");
 //Middlewares
-//cada que un cliente envia una peticion
+//cada vez que un cliente envia una peticion
 //por ejemplo morgan
-app.use(morgan("dev")); //para mostrar mensajes en consola
+app.use(morgan("dev")); //para mostrar mensajes en consola con las solicitudes y que valor retorna
+app.use(express.urlencoded({ extended: false })); //aceptar desde el formulario los datos del usuario
+app.use(express.json());
 
 //Variables Globales
+app.use((req, res, next) => {
+  //toma el pedido del usuario con req
+  //toma lo que quiere responder el servidor
+  //next continua con el resto del código
+  next();
+});
 
 //Routes
 app.use(require("./routes/index.js"));
+app.use(require("./routes/authentication.js"));
 
 //Archivos Publicos (codigo que el navegador puede acceder)
+app.use(express.static(path.join(__dirname, "public")));
 
 //Inicialización del servidor
 app.listen(app.get("port"), () => {
---
+++ src/routes/authentication.js
@@ -0,0 +1,5 @@
+//rutas relacionadas con el loggeo
+const express = require("express");
+const router = express.Router();
+
+module.exports = router;
