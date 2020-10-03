c7ab2ca88f79f3c48fc1f069e499c912cab19628
Fede <fedebenitez1696@gmail.com>
2020-10-03 19:33:15 -0300

    base de datos modificada

--- database/pacientes.sql
+++ database/pacientes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 29-09-2020 a las 17:35:23
+-- Tiempo de generación: 04-10-2020 a las 00:10:47
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.10
 
@@ -24,25 +24,143 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `ficha_paciente`
+--
+
+CREATE TABLE `ficha_paciente` (
+  `dni` varchar(8) COLLATE utf8_spanish_ci NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  `apellido` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  `fecha_nacimiento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `obra_social`
+--
+
+CREATE TABLE `obra_social` (
+  `cod_obra_social` int(11) NOT NULL,
+  `nombre` varchar(200) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `obra_social_paciente`
+--
+
+CREATE TABLE `obra_social_paciente` (
+  `dni_paciente` varchar(11) COLLATE utf8_spanish_ci NOT NULL,
+  `cod_obra_social` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestaciones`
+--
+
+CREATE TABLE `prestaciones` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turno`
+--
+
+CREATE TABLE `turno` (
+  `id` int(11) NOT NULL,
+  `dia` text COLLATE utf8_spanish_ci NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fin` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turno_paciente`
+--
+
+CREATE TABLE `turno_paciente` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_prestacion` int(11) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `email` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `password` text COLLATE utf8_spanish_ci NOT NULL
+  `password` text COLLATE utf8_spanish_ci NOT NULL,
+  `rol` varchar(20) COLLATE utf8_spanish_ci NOT NULL DEFAULT 'paciente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `email`, `password`) VALUES
-(1, 'nico@gmail.com', '$2a$10$tVUnil.hkyOfOBkxikf.EumTDoJ.K8Vd9vHyPAxDw9PwkycU6N2qu');
+INSERT INTO `usuario` (`id`, `email`, `password`, `rol`) VALUES
+(1, 'nico@gmail.com', '$2a$10$tVUnil.hkyOfOBkxikf.EumTDoJ.K8Vd9vHyPAxDw9PwkycU6N2qu', 'paciente'),
+(2, 'fedebenitez1996@hotmail.com', '$2a$10$Uv9EBnF7NWf481pdWeSZFeYQOHJn8O3rjDGreVIZyEqVUQUHPYtAm', 'paciente'),
+(5, 'admin@admin.com', '1234', 'admin');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `ficha_paciente`
+--
+ALTER TABLE `ficha_paciente`
+  ADD PRIMARY KEY (`dni`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `obra_social`
+--
+ALTER TABLE `obra_social`
+  ADD PRIMARY KEY (`cod_obra_social`);
+
+--
+-- Indices de la tabla `obra_social_paciente`
+--
+ALTER TABLE `obra_social_paciente`
+  ADD KEY `cod_obra_social` (`cod_obra_social`),
+  ADD KEY `id_paciente` (`dni_paciente`);
+
+--
+-- Indices de la tabla `prestaciones`
+--
+ALTER TABLE `prestaciones`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `turno`
+--
+ALTER TABLE `turno`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `turno_paciente`
+--
+ALTER TABLE `turno_paciente`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `turno_paciente_ibfk_1` (`id_prestacion`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `usuario`
@@ -55,10 +173,52 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `prestaciones`
+--
+ALTER TABLE `prestaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `turno`
+--
+ALTER TABLE `turno`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `turno_paciente`
+--
+ALTER TABLE `turno_paciente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `ficha_paciente`
+--
+ALTER TABLE `ficha_paciente`
+  ADD CONSTRAINT `ficha_paciente_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
+
+--
+-- Filtros para la tabla `obra_social_paciente`
+--
+ALTER TABLE `obra_social_paciente`
+  ADD CONSTRAINT `obra_social_paciente_ibfk_1` FOREIGN KEY (`cod_obra_social`) REFERENCES `obra_social` (`cod_obra_social`),
+  ADD CONSTRAINT `obra_social_paciente_ibfk_2` FOREIGN KEY (`dni_paciente`) REFERENCES `ficha_paciente` (`dni`);
+
+--
+-- Filtros para la tabla `turno_paciente`
+--
+ALTER TABLE `turno_paciente`
+  ADD CONSTRAINT `turno_paciente_ibfk_1` FOREIGN KEY (`id_prestacion`) REFERENCES `prestaciones` (`id`),
+  ADD CONSTRAINT `turno_paciente_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
