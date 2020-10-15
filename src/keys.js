e687e3158edcfe1c73b4cd150a212672fd7211a4
Fede <fedebenitez1696@gmail.com>
2020-10-15 10:56:30 -0300

    nueva base de datos

--- src/keys.js
+++ src/keys.js
@@ -2,9 +2,9 @@
 //numero de puerto
 module.exports = {
   database: {
-    host: "sql10.freemysqlhosting.net",
-    user: "sql10369633",
-    password: "ysarmG9Du9",
-    database: "sql10369633"
+    host: "lgg2gx1ha7yp2w0k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "z101dnuri4f2dp62",
+    password: "xemkd936q7nug5pr",
+    database: "xcio75te7dp3awav"
   }
 };
