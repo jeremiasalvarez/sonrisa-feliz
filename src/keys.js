7fc0145875f4dde18b22910b9421bc892617f883
Jeremias Alvarez <jerealvarez34@gmail.com>
2021-02-09 19:49:34 -0300

    nueva db

--- src/keys.js
+++ src/keys.js
@@ -1,11 +1,17 @@
 //almacenar palabras claves
 //numero de puerto
+
+const dbName = process.env.DB_NAME,
+  dbHost = process.env.DB_HOST,
+  dbPw = process.env.DB_PW,
+  dbUser = process.env.DB_USER
+
 module.exports = {
   database: {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "xcio75te7dp3awav",
-    port: 3308
+    host: dbHost || "localhost",
+    user: dbUser || "root",
+    password: dbPw || "",
+    database: dbName || "xcio75te7dp3awav",
+    port: 3306
   }
 };
